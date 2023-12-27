81c5d81fa03360de5c733b645b5e14e4336d3d5c
hanielss <haniellemes@hotmail.com>
2023-12-27 19:19:53 -0300

    First-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+/* JSX é quando conseguimos colocar o JavaScript e HTML dentro das funções para criar 
+   Classes no React, preciso utilizar a tag: className="" e não somente Class="" */
+
+// PARA CRIAR ESTADOS NO REACT, UTILIZAMOS OS HOOKS => FERRAMENTAS AUXILIARES, ex.: useState
+
+// Use ref é utilizado também para colher as informações do input, é mais performática.
+
+// Neste projeto estou utilizando a biblioteca AXIOS para conseguir consumir APIs de outro projeto, assim sincronizando Back-end e Front-end
+
+
+
+
+
+import React, { useState, useRef } from 'react';
+import axios from 'axios';
+
+import People from './assets/people.svg'
+import Arrow from './assets/arrow.svg'
+import Trash from './assets/trash.svg'
+
+
+import { Container, H1, Image, ContainerItens, InputLabel, Input, Button, User } from "./styles";
+
+
+const App = () => {
+
+
+  const [users, setUsers] = useState([]);
+  
+  
+  const inputName = useRef()
+  const inputAge = useRef()
+
+
+  const addNewUser = async () => {
+
+    const { data: newUser } = await axios.post('http://localhost:3001/users', {
+
+      name: inputName.current.value,
+      age: inputAge.current.value,
+
+    });
+
+    console.log(newUser)
+
+    setUsers([...users, newUser]);
+
+  }
+
+
+  const deleteUser = (userId) => {
+
+    const newUsers = users.filter(user => user.id !== userId)
+
+    setUsers(newUsers)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Container>
+
+      <Image alt="logo-imagem" src={People} />
+
+      <ContainerItens>
+
+        <H1>Olá!</H1>
+
+        <InputLabel>Nome</InputLabel>
+        <Input ref={inputName} placeholder='Nome' />
+
+        <InputLabel>Idade</InputLabel>
+        <Input ref={inputAge} placeholder='Idade' />
+
+        <Button onClick={addNewUser}>Cadastrar <img alt="seta" src={Arrow} /></Button>
+
+        <ul>
+
+          {users.map((user) => (
+
+            <User key={user.id}>
+              <p>{user.name}</p> <p>{user.age}</p>
+              <button onClick={() => deleteUser(user.id)} ><img src={Trash} alt="lata-de-lixo"></img></button>
+
+            </User>
+
+          ))}
+
+        </ul>
+
+      </ContainerItens>
+
+    </Container>
+
   );
+
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,161 @@
+
+/* Para conseguir utilizar o css no React, precisamos sempre baixar uma biblioteca 
+ u/tilizando YARN ADD styled-components, depois inserir os códigos abaixo para conseguir conectar
+ /ambos (importar e exportar)  projeto iniciado dia 07/12/2023 */
+
+import styled from "styled-components";
+
+import Background from './assets/background1.svg'
+
+
+export const Container = styled.div`    
+    background: url("${Background}");
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 44px; 
+    height: 130vh;
+
+`;
+
+
+export const Image = styled.img`
+
+margin-top: 30px;
+
+
+
+`;
+
+export const ContainerItens = styled.div`
+
+background: linear-gradient(
+    157.44deg,
+     rgba(255, 255, 255, 0.6) 0.84%,
+     rgba(255, 255, 255, 0.6) 0.85%, 
+     rgba(255, 255, 255, 0.15) 100%);      
+     border-radius: 61px 61px 0px 0px;
+      padding: 50px 36px;
+      display: flex;
+      flex-direction: column;
+      height: 100vh;
+
+`;
+
+
+export const H1 = styled.h1`
+
+color: #ffffff;
+font-size: 34px;
+font-weight: 700;
+line-height: 40px;
+letter-spacing: 0px;
+text-align: center;
+font-style: normal;
+
+
+`;
+
+
+export const InputLabel = styled.p`
+
+font-size: 18px;
+font-weight: bold;
+line-height: 22px;
+letter-spacing: -0.408px;
+text-align: left;
+color: #eeeeee;
+font-style: normal;
+margin-left: 25px;
+
+
+`;
+
+export const Input = styled.input`
+box-shadow: 0px 4px 4px 0px #00000040;
+background: #FFFFFF40;
+border-radius: 14px;
+border: none;
+width: 342px;
+height: 58px;
+outline: none;
+padding-left: 25px;
+margin-bottom: 34px;
+font-size: 20px;
+font-weight: normal;
+font-style: normal;
+line-height: 28px;
+color: #FFFFFF;
+
+
+`;
+
+export const Button = styled.button`
+background: #000000CC;
+color: #FFFFFF;
+width: 342px;
+height: 74px;
+border-radius: 14px;
+font-size: 17px;
+font-weight: 700;
+line-height: 28px;
+letter-spacing: 0px;
+border: none;
+outline: none;
+cursor: pointer;
+display: flex;
+align-items: center;
+justify-content: center;
+gap: 10px;
+
+&:hover {
+    opacity: 0.8;
+}
+&:active {
+    opacity: 0.5;
+}
+
+
+`;
+
+export const User = styled.li `
+
+box-shadow: 0px 4px 4px 0px #00000040;
+background: #FFFFFF40;
+border-radius: 14px;
+border: none;
+width: 342px;
+height: 58px;
+outline: none;
+margin-top: 20px;
+display: flex;
+justify-content: space-around;
+align-items: center;
+
+Button {
+    background:none;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    &:hover {
+    opacity: 0.8;
+}
+&:active {
+    opacity: 0.5;
+}
+    
+}
+
+P {
+    color: #FFFFFf;
+    font-size: 17px;
+    font-weight: 700;
+    line-height: 28px;
+    letter-spacing: 0px;
+}
+
+
+`
+
+
